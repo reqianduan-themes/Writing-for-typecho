ea3e7ca2b6d2d40982ca4f284221a3a8215ea002
arvin <webappsniper@gmail.com>
2014-08-28 17:54:14 +0800

    增加css模块注释

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <!--
 ******主题信息******
 * Theme Name: Writing for typecho
-* Theme URI: http://themes.xiguabaobao.com/Writing/
+* Theme URI: http://themes.xiguabaobao.com/Writing-for-typecho/
 * Description: Writing主题（免费） BUG反馈：<a href="http://themes.xiguabaobao.com/Writing-for-typecho/#respond">西瓜宝宝主题</a>
 * Author: 西瓜宝宝主题
 * Author URI: http://themes.xiguabaobao.com
